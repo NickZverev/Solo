c86fb790eeab27396f1e121c4c892c73023d2b6b
Nikolai <ZverevNA12345@outlook.com>
2022-09-26 02:03:08 +0300

    add Task_5_range

---
+++ Task_5_range/Program.cs
@@ -0,0 +1,12 @@
+﻿// 5. Напишите программу, которая на вход принимает одно
+// число (N), а на выходе показывает все целяе числа в 
+// промежутке от (-N) до (N).
+
+int num = Convert.ToInt32(Console.ReadLine());
+int counter = - num;
+while (counter <= num)
+{
+    Console.Write($"{counter} ");
+    counter++;
+}
+
