2b09a81a6460ba0558da8e592ac8a15b717ca50c
Nikolai <ZverevNA12345@outlook.com>
2022-09-25 17:05:34 +0300

    add Task_0_sguare

---
+++ Task_0_sguare/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число
+// и выдаёт его квадрат (число, умноженное на само себя)
+// Например:    4 -> 16,    -3 -> 9,    -7 -> 49
+
+Console.Write("Введите целое число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+int sguare = namber * namber;
+Console.WriteLine($"Квадрат числа {namber} = {sguare}");
