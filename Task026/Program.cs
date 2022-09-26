35b626549c0c059e22009606f3dbb4469c9204eb
Nikolai <ZverevNA12345@outlook.com>
2022-09-26 13:51:09 +0300

    add Task026

---
+++ Task026/Program.cs
@@ -0,0 +1,17 @@
+﻿// 26/ Напишите программу, которая принимает на вход число и
+// выдайт количество цифр в числе
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = DigitQuantity(number);
+Console.WriteLine($"В числе {number} - {result} цифр ");
+int DigitQuantity (int num)
+{
+    int counter = 0;
+    while(num > 0)
+    {
+        num = num / 10;
+        counter++;
+    }
+    return counter;
+}
