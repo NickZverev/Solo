2a9a5c5b1ccc0dc059477ea3bd6b60d19e21990a
Nikolai <ZverevNA12345@outlook.com>
2022-09-28 02:50:11 +0300

    add Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,10 @@
+﻿// 6. Напишите программу, которая на вход принимает число
+// и выдаёт, является ли число чётным (делится ли она на 
+// два без остатка). Например:  4->да,  -3->нет,  7->нет
+
+Console.Write("Введите число  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0) Console.WriteLine($"{number} -> да");
+
+else Console.WriteLine($"{number} -> нет");
