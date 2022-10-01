ff00f4d9074874689f62cafcab9e9d6d0733761f
Nikolai <ZverevNA12345@outlook.com>
2022-10-02 00:15:46 +0300

    add Task023

---
+++ Task023/Program.cs
@@ -0,0 +1,18 @@
+﻿// 23. Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27       5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void SquareTable(int number)
+{
+    int counter = 1;
+    while (counter <= number)
+    {
+        Console.WriteLine($"|{counter, 3}|{counter * counter * counter, 3}|");
+        counter++;
+    }
+}
+if (num > 0) SquareTable(num);
+else Console.WriteLine("Введено некорректное значение");
