75992fa41f4d4959f6d19117a7a2011f2076ebce
Nikolai <ZverevNA12345@outlook.com>
2022-10-28 12:17:16 +0300

    add Task066

---
+++ Task066/Program.cs
@@ -0,0 +1,17 @@
+﻿// 66: Задайте значения М и N. Напишите программу, которая
+// найдёт сумму натуральных элементов в промежутке от М до N.
+// М = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+Console.Write("Введите значение M:\t");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N:\t");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = SumRec(m, n);
+Console.Write($"Сумма чисел от M до N = {SumRec(m, n)}");
+
+int SumRec(int m, int n)
+{
+    if(n == m) return m;
+    else return n + SumRec(m, n - 1);
+}
