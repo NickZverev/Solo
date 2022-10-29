9f0c2bdf3e00c0eace6aed36a966ede10b362fff
Nikolai <ZverevNA12345@outlook.com>
2022-10-29 06:20:03 +0300

    add changes Task066

--- Task066/Program.cs
+++ Task066/Program.cs
@@ -7,7 +7,20 @@ Console.Write("Введите значение M:\t");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение N:\t");
 int n = Convert.ToInt32(Console.ReadLine());
+
+// Проверка корректности введённых значений
+if(m > n)
+{
+    int temp = m;
+    m = n;
+    n = temp;
+}
+Console.Write($"Проверка значения М:\t{m}");
+Console.WriteLine();
+Console.Write($"Проверка значения N:\t{n}");
+
 int sum = SumRec(m, n);
+Console.WriteLine();
 Console.Write($"Сумма чисел от M до N = {SumRec(m, n)}");
 
 int SumRec(int m, int n)
